782f905f7a425cad8e75af2505dd6751c09c3ea0
Jude Lawson <lawsonjdutton@gmail.com>
2018-05-15 14:17:06 -0600

    Updates student migration to include missing timestamps

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2018_05_15_193438) do
   create_table "students", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
