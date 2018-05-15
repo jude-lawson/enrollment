1eb36a890d10acdc5b84b75bf945f60edcef79b3
Jude Lawson <lawsonjdutton@gmail.com>
2018-05-15 14:52:18 -0600

    Implements address model, validates, and creates table

---
+++ db/migrate/20180515205131_create_addresses.rb
@@ -0,0 +1,13 @@
+class CreateAddresses < ActiveRecord::Migration[5.2]
+  def change
+    create_table :addresses do |t|
+      t.text :description
+      t.string :street_address
+      t.string :city
+      t.string :state
+      t.integer :zip_code
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_15_193438) do
+ActiveRecord::Schema.define(version: 2018_05_15_205131) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "addresses", force: :cascade do |t|
+    t.text "description"
+    t.string "street_address"
+    t.string "city"
+    t.string "state"
+    t.integer "zip_code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "students", force: :cascade do |t|
     t.string "first_name"
